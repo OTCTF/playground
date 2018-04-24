9eea51bcc59cfb96e4ecee06395cd75b0359ebb2
lixianliang <lixianliang@corp.netease.com>
2018-04-24 16:31:30 +0800

    ./run.sh start 直接进入交互模式

--- run.sh
+++ run.sh
@@ -1,59 +1,30 @@
 #!/bin/bash
 
-#! /bin/bash
-cd `dirname "$0"`
-PATH_DIR=`pwd`
+cd `dirname $0`
+THIS_DIR=`pwd`
 
-ENV_DIR=$PATH_DIR
-
-setIPEnv() {
-    export TEST_SEVER="223.252.216.178"
-    if [[ `uname -a | awk '{print $1}'` = 'Darwin' ]]; then
-       export IP_ADDRESS=`ifconfig en0 | grep 'inet '| cut -d' ' -f2|head -1`
-    fi
-    if [[ `uname -a | awk '{print $1}'` = 'Linux' ]]; then
-       export IP_ADDRESS=`ifconfig enp3s0 | grep 'inet '| awk '{print $2}' | sed 's/addr://g'`
-    fi
-
-    if [[ ! -n $IP_ADDRESS ]]; then
-       echo ""
-       echo "START ERROR!"
-       echo "======================================"
-       echo "启动失败，需要ip地址，可以尝试使用『虚拟地址』"
-       echo ""
-       echo "eg:"
-       echo "macOS run    'sudo ifconfig en0 alias 172.31.254.254 255.255.255.0'"
-       echo "Linux run    'sudo ifconfig eth0:0 172.31.254.254'"
-       echo ""
-       exit 1
-    fi
-    echo 'current ip address: '$IP_ADDRESS
+_enter() {
+    docker exec -it `docker ps -q | head -n 1` /bin/bash
 }
 
-up() {
-    setIPEnv
-    cd $ENV_DIR
+start() {
+    cd $THIS_DIR
     docker-compose up -d $@
+    _enter
 }
 
-down() {
-    cd $ENV_DIR
+stop() {
+    cd $THIS_DIR
     docker-compose down
 }
 
-restart() {
-    setIPEnv
-    cd $ENV_DIR
-    docker-compose restart $@
-}
-
 case "$1" in
-        up|restart|down)
+        start|stop)
             COMMAND=$1
             shift
             $COMMAND $@
             ;;
         *)
-            echo $"Usage: $0 {up|restart|down}"
+            echo $"Usage: $0 {start|stop}"
             exit 1
 esac
