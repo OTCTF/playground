cbe8f5245666203200fdb463ae9c97c34f55737d
lixianliang <lixianliang@corp.netease.com>
2018-07-10 11:00:23 +0800

    add php5 env; add signsrch; add winspy; add ida pro 7.0 remote debug windows server

--- php5.sh
+++ php5.sh
@@ -4,25 +4,25 @@ cd `dirname $0`
 THIS_DIR=`pwd`
 
 _enter() {
-    docker exec -it `docker ps | grep 'otctf/default:latest' | awk '{print $1}'` /bin/bash
+    docker exec -it `docker ps | grep 'otctf/default:php5' | awk '{print $1}'` /bin/bash
 }
 
 start() {
     cd $THIS_DIR
-    docker-compose up -d $@
+    docker-compose up -d -f ./docker-compose-php5.yml
     _enter
 }
 
 stop() {
     cd $THIS_DIR
-    docker-compose down
+    docker-compose down -f ./docker-compose-php5.yml
 }
 
 case "$1" in
         start|stop)
             COMMAND=$1
             shift
-            $COMMAND $@
+            $COMMAND
             ;;
         *)
             echo $"Usage: $0 {start|stop}"
